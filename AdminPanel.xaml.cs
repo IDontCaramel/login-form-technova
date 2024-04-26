f571b5809118cb5b085e3fac6b0b8e33a12b3069
IDontCaramel <lucaskoot@icloud.com>
2024-04-26 12:58:18 +0200

    initial commit

---
+++ AdminPanel.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Definitions.Charts;
+using LiveCharts.Wpf;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using SkiaSharp;
+
+namespace login_form_technova
+{
+    public partial class AdminPanel : Window
+    {
+        private string csvPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "opendag_inschrijvingen\\inschijvingen.csv");
+        private DataTable dataTable;
+        private SeriesCollection seriesCollection = new SeriesCollection();
+
+
+        public AdminPanel()
+        {
+            InitializeComponent();
+            LoadCsvData();
+
+            string[] headers;
+            string[,] dataArray = ReadCsvToArray(csvPath, out headers);
+
+            ProcessEachRow(dataArray);
+
+            
+
+
+        }
+
+        private void ProcessEachRow(string[,] array)
+        {
+            int rowCount = array.GetLength(0);
+            int colCount = array.GetLength(1);
+
+            Dictionary<string, Dictionary<string, int>> dateToChoiceCounts = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 1; i < rowCount; i++)
+            {
+                string date = array[i, 3]; 
+                string choice = array[i, 5]; 
+
+                if (!string.IsNullOrEmpty(date) && !string.IsNullOrEmpty(choice))
+                {
+                    if (!dateToChoiceCounts.ContainsKey(date))
+                    {
+                        dateToChoiceCounts[date] = new Dictionary<string, int>();
+                    }
+
+                    if (dateToChoiceCounts[date].ContainsKey(choice))
+                    {
+                        dateToChoiceCounts[date][choice]++;
+                    }
+                    else
+                    {
+                        dateToChoiceCounts[date][choice] = 1;
+                    }
+                }
+            }
+
+            TextBox.Clear();
+
+            foreach (var kvp in dateToChoiceCounts)
+            {
+                
+
+                TextBox.AppendText($"Date: {kvp.Key}\n");
+                foreach (var choiceCount in kvp.Value)
+                {
+                    TextBox.AppendText($"Choice: {choiceCount.Key}, Count: {choiceCount.Value}\n");
+                }
+                TextBox.AppendText("\n"); 
+            }
+        }
+
+
+
+
+
+
+
+        private void LoadCsvData()
+        {
+            try
+            {
+                string[] headers;
+                string[,] dataArray = ReadCsvToArray(csvPath, out headers);
+                dataTable = ConvertArrayToDataTable(dataArray, headers);
+                csvData.ItemsSource = dataTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading CSV file: " + ex.Message);
+            }
+        }
+
+        private string[,] ReadCsvToArray(string filePath, out string[] headers)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            headers = lines[0].Split(',');
+            int rowCount = lines.Length;
+            int colCount = headers.Length;
+
+            string[,] dataArray = new string[rowCount - 1, colCount];
+
+            for (int i = 1; i < rowCount; i++)
+            {
+                string[] fields = lines[i].Split(',');
+                for (int j = 0; j < colCount; j++)
+                {
+                    if (j < fields.Length)
+                        dataArray[i - 1, j] = fields[j];
+                    else
+                        dataArray[i - 1, j] = "";
+                }
+            }
+            return dataArray;
+        }
+
+        private DataTable ConvertArrayToDataTable(string[,] array, string[] headers)
+        {
+            DataTable dataTable = new DataTable();
+            int rowCount = array.GetLength(0);
+            int colCount = array.GetLength(1);
+
+            foreach (string header in headers)
+            {
+                dataTable.Columns.Add(header);
+            }
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                DataRow newRow = dataTable.NewRow();
+                for (int j = 0; j < colCount; j++)
+                {
+                    newRow[j] = array[i, j];
+                }
+                dataTable.Rows.Add(newRow);
+            }
+
+            return dataTable;
+        }
+
+        private void csvData_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            var editedTextBox = e.EditingElement as TextBox;
+            var newValue = editedTextBox.Text;
+
+            int rowIndex = e.Row.GetIndex();
+            int colIndex = e.Column.DisplayIndex;
+
+            dataTable.Rows[rowIndex][colIndex] = newValue;
+
+            SaveDataTableToCsv(csvPath, dataTable);
+        }
+
+        private void SaveDataTableToCsv(string filePath, DataTable dataTable)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(string.Join(",", dataTable.Columns));
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    var fields = row.ItemArray;
+                    writer.WriteLine(string.Join(",", fields));
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace login_form_technova
+{
+  
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtName = InputName;
+            txtPhone = InputPhone;
+            txtEmail = InputMail;
+
+
+            userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            newDirectoryPath = System.IO.Path.Combine(userDirectory, "opendag_inschrijvingen");
+            
+
+        }
+
+
+        private TextBox txtName;
+        private TextBox txtPhone;
+        private TextBox txtEmail;   
+        private string Chosendate;
+        private string ChosenOpleiding;
+        private string userDirectory;
+        private string newDirectoryPath;
+        private string pwHash = "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9";
+        private int validInputsCount = 0;
+
+        private void InputName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtName = (TextBox)sender;  
+        }
+
+        private void InputMail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtEmail = (TextBox)sender;
+        }
+
+        private void InputPhone_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            foreach (char c in e.Text)
+            {
+                if (!char.IsDigit(c))
+                {
+                    e.Handled = true; 
+                    return;
+                }
+            }
+        }
+
+        private void InputPhone_lostfocus(object sender, RoutedEventArgs e)
+        {
+            txtPhone = (TextBox)sender;
+
+            string phoneNumber = txtPhone.Text;
+
+            txtPhone.Text = FormatPhoneNumber(phoneNumber);
+        }
+
+        private string FormatPhoneNumber(string phoneNumber)
+        {
+            if (phoneNumber == null)
+                return string.Empty;
+
+            switch (phoneNumber.Length)
+            {
+                case 10:
+                    return Regex.Replace(phoneNumber, @"(\d{2})(\d{4})(\d{4})", "($1)-$2-$3");
+                case 9:
+                    return Regex.Replace(phoneNumber, @"(\d{3})(\d{3})(\d{3})", "($1)-$2-$3");
+                case 11:
+                    return Regex.Replace(phoneNumber, @"(\d{3})(\d{4})(\d{4})", "($1)-$2-$3");
+                default:
+                    return phoneNumber;
+            }
+        }
+
+        private int ExtraPeople()
+        {
+            if (_1extra.IsChecked == true)
+            {
+                return 1;
+            }
+            else if (_2extra.IsChecked == true)
+            {
+                return 2;
+            }
+            
+            else 
+            { 
+                return 0;
+            }
+        }
+
+        private void cbDate_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            if (comboBox != null && comboBox.SelectedItem != null)
+            {
+                ComboBoxItem selectedItem = comboBox.SelectedItem as ComboBoxItem;
+                if (selectedItem != null)
+                {
+                    Chosendate = selectedItem.Content.ToString();
+                }
+            }
+        }
+
+
+        private void cbOpleiding_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            if (comboBox != null && comboBox.SelectedItem != null)
+            {
+                ComboBoxItem selectedItem = comboBox.SelectedItem as ComboBoxItem;
+                if (selectedItem != null)
+                {
+                    ChosenOpleiding = selectedItem.Content.ToString();
+                }
+            }
+        }
+
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckInputs() == true)
+            {
+                SaveResults();
+                MessageBox.Show("succes");
+
+            }
+            else
+            {
+                MessageBox.Show("ful al de velden in");
+            }
+
+        }
+
+
+
+        private bool CheckInputs()
+        {
+            validInputsCount = 0;
+            bool isValidEmail = IsValidEmail(txtEmail.Text);
+
+            SetLabelColor(txtName.Text, lblName);
+            SetLabelColor(txtEmail.Text, lblMail, isValidEmail);
+            SetLabelColor(txtPhone.Text, lblPhone);
+            SetLabelColor(cbDate.SelectedIndex, lblDate);
+            SetLabelColor(cbOpleiding.SelectedIndex, lblOpleiding);
+
+            if (validInputsCount >= 5)
+            {
+                validInputsCount = 0;
+                return true;
+            }
+            else
+            {
+                validInputsCount = 0;
+                return false;
+            }
+        }
+
+        private void SetLabelColor(string input, Label label, bool isValidEmail = true)
+        {
+            if (string.IsNullOrWhiteSpace(input) || (label != null && !isValidEmail))
+            {
+                label.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                label.Foreground = new SolidColorBrush(Colors.Black);
+                validInputsCount++;
+            }
+        }
+
+        private void SetLabelColor(int selectedIndex, Label label)
+        {
+            if (selectedIndex == 0 && label != null)
+            {
+                label.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                label.Foreground = new SolidColorBrush(Colors.Black);
+                validInputsCount++;
+            }
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            var trimmedEmail = email.Trim();
+            if (trimmedEmail.EndsWith("."))
+            {
+                return false;
+            }
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == trimmedEmail;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+
+        private bool SaveResults()
+        {
+            string csv = System.IO.Path.Combine(newDirectoryPath, "inschijvingen.csv");
+
+            try
+            {
+                if (!Directory.Exists(newDirectoryPath))
+                {
+                    Directory.CreateDirectory(newDirectoryPath);
+                }
+
+                if (!File.Exists(csv))
+                {
+                    using (StreamWriter headerWriter = new StreamWriter(csv))
+                    {
+                        headerWriter.WriteLine("Name,Email,Phone,Date,Extra,Opleiding");
+                    }
+                }
+
+                using (StreamWriter sw = File.AppendText(csv))
+                {
+                    string name = txtName.Text;
+                    string email = txtEmail.Text;
+                    string phone = txtPhone.Text;
+                    string date = Chosendate;
+                    string extra = ExtraPeople().ToString();
+                    string Opleiding = ChosenOpleiding;
+
+                    string newLine = $"{name},{email},{phone},{date},{extra},{Opleiding}";
+                    sw.WriteLine(newLine);
+                }
+
+
+                return true; 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return false;
+            }
+        }
+
+        private void btnAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            InputBox inputBox = new InputBox();
+            bool? result = inputBox.ShowDialog();
+
+            if (result == true)
+            {
+                string userInput = inputBox.UserInput;
+                if (Hash(userInput) == true) 
+                {
+                    AdminPanel adminPanel = new AdminPanel();
+                    adminPanel.Show();
+                }
+                else
+                {
+                    MessageBox.Show("wrong password");
+                }
+            }
+        }
+
+        private bool Hash(string hash)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(hash));
+
+                string hashString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+                if (hashString == pwHash)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        
+    }
+}
